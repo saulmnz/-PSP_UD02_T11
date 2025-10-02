aa5b43b4182f55fcd69c15821a61f8953026413e
saulmnz <salvarezmonzon@danielcastelao.org>
2025-10-02 10:08:49 +0200

    NIVEL 2

--- T11_DIEGO/src/Domino.java
+++ T11_DIEGO/src/Domino.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Domino {
     public static void main(String[] args) {
+        Scanner MiScanner = new Scanner(System.in);
+        int N = 5; // valor por defecto en caso de que el valor introducido no sea válido
 
-        Thread hilo5 = new Fichita("Hilo-5", null);
-        Thread hilo4 = new Fichita("Hilo-4", hilo5);
-        Thread hilo3 = new Fichita("Hilo-3", hilo4);
-        Thread hilo2 = new Fichita("Hilo-2", hilo3);
-        Thread hilo1 = new Fichita("Hilo-1", hilo2);
+        System.out.print("Cuántas fichas de dominó, mínimo 1 ;) !! : ");
+        try {
+            N = MiScanner.nextInt();
+            if (N < 1) {
+                System.out.println("Te dije que no puede ser menor que 1. Se usarán 5 por defecto.");
+                N = 5;
+            }
+        } catch (Exception e) {
+            System.out.println("NO VÁLIDO, Usaré 5 por defecto.");
+            N = 5;
+        }
+        MiScanner.close();
 
-        hilo1.start();
+        // cadena de hilos, lectura: atras -> adelante
+        Thread siguiente = null;
+        for (int i = N; i >= 1; i--) {
+            String nombre = "Hilo-" + i;
+            Thread nuevo = new Fichita(nombre, siguiente);
+            siguiente = nuevo;
+        }
 
+        // último hilo creado, primero en la cadena (Hilo-1)
+        if (siguiente != null) {
+            siguiente.start();
+        }
     }
 }
